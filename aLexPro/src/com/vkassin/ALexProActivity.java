d2e686d2ba45d26101a1e88e95da7b0939c696c7
Vadim <vgzulus@gmail.com>
2012-02-29 17:20:40 +0600

    o

--- aLexPro/src/com/vkassin/ALexProActivity.java
+++ aLexPro/src/com/vkassin/ALexProActivity.java
@@ -1,15 +1,97 @@
 package com.vkassin;
 
-//1
-
-import android.app.Activity;
+import android.app.TabActivity;
+import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TabHost;
+import android.widget.TabWidget;
+import android.widget.TextView;
 
-public class ALexProActivity extends Activity {
+public class ALexProActivity extends TabActivity {
     /** Called when the activity is first created. */
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+	public void onCreate(Bundle savedInstanceState) {
+		
+	    super.onCreate(savedInstanceState);
+	    setContentView(R.layout.main);
+
+//	    Common.app_ctx = getApplicationContext();
+	    
+ //       Common.prepareUserAgent(this);
+
+	    Resources res = getResources(); // Resource object to get Drawables
+	    TabHost tabHost = getTabHost();  // The activity TabHost
+	    TabHost.TabSpec spec;  // Reusable TabSpec for each tab
+	    Intent intent;  // Reusable Intent for each tab
+
+	    // Create an Intent to launch an Activity for the tab (to be reused)
+	    intent = new Intent().setClass(this, MainActivity.class);
+
+	    // Initialize a TabSpec for each tab and add it to the TabHost
+	    spec = tabHost.newTabSpec("tmain").setIndicator("Main",
+	                      res.getDrawable(R.drawable.house))
+	                  .setContent(intent);
+	    tabHost.addTab(spec);
+
+	    // Do the same for the other tabs
+	    intent = new Intent().setClass(this, MailActivity.class);
+	    spec = tabHost.newTabSpec("tmail").setIndicator("Mail",
+	                      res.getDrawable(R.drawable.chat2))
+	                  .setContent(intent);
+	    tabHost.addTab(spec);
+
+	    intent = new Intent().setClass(this, FavrActivity.class);
+	    spec = tabHost.newTabSpec("tfavr").setIndicator("Избранное",
+	                      res.getDrawable(R.drawable.star))
+	                  .setContent(intent);
+	    tabHost.addTab(spec);
+
+	    intent = new Intent().setClass(this, PodActivity.class);
+	    spec = tabHost.newTabSpec("tpod").setIndicator("Подкасты",
+	                      res.getDrawable(R.drawable.note))
+	                  .setContent(intent);
+	    tabHost.addTab(spec);
+
+	    intent = new Intent().setClass(this, SettActivity.class);
+	    spec = tabHost.newTabSpec("tsett").setIndicator("Настройки",
+	                      res.getDrawable(R.drawable.gear))
+	                  .setContent(intent);
+	    tabHost.addTab(spec);
+	  
+	    LinearLayout ll = (LinearLayout) tabHost.getChildAt(0);
+	    TabWidget tw = (TabWidget) ll.getChildAt(0);
+	    RelativeLayout rllf = (RelativeLayout) tw.getChildAt(0);
+//	    rllf.setBackgroundColor(0xFF0000FF);
+	    TextView lf = (TextView) rllf.getChildAt(1);
+	    lf.setTextSize(textsize);
+	    lf.setPadding(0, 0, 0, 2);
+	    rllf = (RelativeLayout) tw.getChildAt(1);
+//	    rllf.setBackgroundColor(0xFF0000FF);
+	    lf = (TextView) rllf.getChildAt(1);
+	    lf.setTextSize(textsize);
+//	    lf.setLines(2);
+//	    lf.setInputType(InputType.TYPE_TEXT_FLAG_MULTI_LINE);
+	    lf.setPadding(0, 0, 0, 2);
+	    rllf = (RelativeLayout) tw.getChildAt(2);
+//	    rllf.setBackgroundColor(0xFF0000FF);
+	    lf = (TextView) rllf.getChildAt(1);
+	    lf.setTextSize(textsize);
+	    lf.setPadding(0, 0, 0, 2);
+	    rllf = (RelativeLayout) tw.getChildAt(3);
+//	    rllf.setBackgroundColor(0xFF0000FF);
+	    lf = (TextView) rllf.getChildAt(1);
+	    lf.setTextSize(textsize);
+	    lf.setPadding(0, 0, 0, 2);
+
+	    rllf = (RelativeLayout) tw.getChildAt(4);
+//	    rllf.setBackgroundColor(0xFF0000FF);
+	    lf = (TextView) rllf.getChildAt(1);
+	    lf.setTextSize(textsize);
+	    lf.setPadding(0, 0, 0, 2);
+	    
+	    tabHost.setCurrentTab(0);
+	}
 }
