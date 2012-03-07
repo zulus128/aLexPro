eddeb6e884197c8e56dff82d8baaa793f2b0838c
Vadim <vgzulus@gmail.com>
2012-03-07 14:44:54 +0600

    e

--- aLexPro/src/com/vkassin/alexpro/MainActivity.java
+++ aLexPro/src/com/vkassin/alexpro/MainActivity.java
@@ -2,23 +2,34 @@ package com.vkassin.alexpro;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 public class MainActivity extends Activity {
 
 	private static final String TAG = "aLexPro.MainActivity"; 
+	WebView engine;
 	
 	public void onCreate(Bundle icicle) {
 
 		super.onCreate(icicle);
 		setContentView(R.layout.mainact);
 		 
-		WebView engine = (WebView) findViewById(R.id.web_engine);
+		engine = (WebView) findViewById(R.id.web_engine);
 		engine.setWebViewClient(new HelloWebViewClient());
 		engine.getSettings().setJavaScriptEnabled(true);
 		engine.loadUrl(Common.WEB_OPEN);
 	}
+    
+	@Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && engine.canGoBack()) {
+            engine.goBack();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
 	
 	private class HelloWebViewClient extends WebViewClient {
 	    @Override
@@ -26,5 +37,6 @@ public class MainActivity extends Activity {
 	        view.loadUrl(url);
 	        return true;
 	    }
+
 	}
 }
