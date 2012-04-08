9b8b225382836427999239211681355fa49a3a4e
Zulus <vgzulus@gmail.com>
2012-04-08 15:07:03 +0600

    rr

--- aLexPro/src/com/vkassin/alexpro/DownloadActivity.java
+++ aLexPro/src/com/vkassin/alexpro/DownloadActivity.java
@@ -1,18 +1,33 @@
 package com.vkassin.alexpro;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStreamReader;
+import java.net.URL;
 import java.util.ArrayList;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.protocol.BasicHttpContext;
+import org.apache.http.protocol.HttpContext;
 
 import android.app.Activity;
 import android.app.ListActivity;
-import android.content.Intent;
+import android.net.ConnectivityManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
+import android.webkit.ConsoleMessage;
+import android.webkit.ValueCallback;
 import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ListView;
 import android.widget.ProgressBar;
-import android.widget.RelativeLayout;
 
 public class DownloadActivity extends ListActivity {
 
@@ -20,12 +35,110 @@ public class DownloadActivity extends ListActivity {
 //	private ListView list;
 	private DownloadArrayAdapter adapter;
 	private ProgressBar pb;
+	private WebView engine;
+	
+	private String ccc;
+	
+	class MyJavaScriptInterface  
+	{  
+	    @SuppressWarnings("unused")  
+	    public void showHTML(String html)  
+	    {  
+//	        new AlertDialog.Builder(myApp)  
+//	            .setTitle("HTML")  
+//	            .setMessage(html)  
+//	            .setPositiveButton(android.R.string.ok, null)  
+//	        .setCancelable(false)  
+//	        .create()  
+//	        .show();
+	    	
+	    	ccc = html;
+	    	
+	    	Log.w(TAG, ccc);
+	    }  
+	} 
 	
 	public void onCreate(Bundle icicle) {
 
 		super.onCreate(icicle);
         setContentView(R.layout.favs);
-                
+            
+		engine = (WebView) findViewById(R.id.web_engine_fav);
+		engine.getSettings().setJavaScriptEnabled(true);
+		engine.getSettings().setDomStorageEnabled(true);
+		engine.getSettings().setAppCacheMaxSize(1024*1024*8);
+//        engine.getSettings().setAppCachePath("/data/data/de.app/cache");
+//		engine.getSettings().setAllowFileAccess(true);
+//		engine.getSettings().setAppCacheEnabled(true);
+		String appCachePath = "/data/data/com.vkassin.alexpro/cache";//getApplicationContext().getCacheDir().getAbsolutePath();
+		engine.getSettings().setAppCachePath(appCachePath);
+		engine.getSettings().setAllowFileAccess(true);
+		engine.getSettings().setAppCacheEnabled(true);
+//		engine.clearCache(false);
+		
+		pb = (ProgressBar)findViewById(R.id.ProgressBar02);
+
+	    engine.setWebChromeClient(new WebChromeClient() {
+			
+	        public boolean onConsoleMessage(ConsoleMessage cmsg)
+	        {
+	            // check secret prefix
+	            if (cmsg.message().startsWith("MAGIC"))
+	            {
+	                String msg = cmsg.message().substring(5); // strip off prefix
+
+	                /* process HTML */
+	                ccc = msg;
+
+	                return true;
+	            }
+
+	            return false;
+	        }
+	        
+			public void onProgressChanged(WebView view, int progress) {
+				
+				if(progress < 100)
+					pb.setVisibility(View.VISIBLE);
+				else {
+					
+					pb.setVisibility(View.GONE);
+					
+//					engine.saveWebArchive(getFilesDir().getAbsolutePath() + File.separator + "my.webarchive");
+//					engine.saveWebArchive("file:///android_asset/qqq");
+					
+//					String savename = "ggg.webarchive";
+//					ValueCallback<String> callback = null;
+//					engine.saveWebArchive(getFilesDir().getAbsolutePath() + File.separator  + savename , true , callback);
+//					engine.saveWebArchive(getFilesDir().getAbsolutePath() + File.separator  + savename);
+//					Log.i("~~~~~~~~~~~~~callback~~~~~~~~~~~~~~","" + callback);
+//					engine.loadUrl("file://" +getFilesDir().getAbsolutePath() + File.separator  + savename);
+
+				}
+					
+//				activity.setProgress(progress * 1000);
+//				Log.w(TAG, ""+progress);
+			}
+		});
+	    
+	    engine.setWebViewClient(new WebViewClient() {
+	    	
+	    	public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+
+	    		pb.setVisibility(View.GONE);
+	    	}
+	    	
+	    	   @Override  
+	    	    public void onPageFinished(WebView view, String url)  
+	    	    {  
+	    	        /* This call inject JavaScript into the page which just finished loading. */  
+//	    	        engine.loadUrl("javascript:window.HTMLOUT.showHTML('<head>'+document.getElementsByTagName('html')[0].innerHTML+'</head>');");
+	    	        Log.w(TAG, "logggg");
+	    	        engine.loadUrl("javascript:console.log('MAGIC'+document.getElementsByTagName('html')[0].innerHTML);");
+//	    	        engine.loadUrl("javascript:console.log('MAGIC'+document.getElementsByTagName('body')[0].outerText);");
+	    	    }  
+	    });
+
 //        list = (ListView)this.findViewById(R.id.FavsList);
 
 //        list.setEmptyView(findViewById(R.id.empty));
@@ -43,7 +156,102 @@ public class DownloadActivity extends ListActivity {
 //		});
 		 
 	}
+	
+	@Override
+	protected void onListItemClick(ListView l, View v, final int position, long id) {
+		super.onListItemClick(l, v, position, id);
+		
+		//Log.i(TAG,"click");
+		final RSSItem it = adapter.getItems().get(position);
+		
+		this.getListView().setVisibility(View.GONE);
+        engine.setVisibility(View.VISIBLE);
+        
+        
+        engine.clearView();
+        
+//		engine.getSettings().setCacheMode(WebSettings.LOAD_CACHE_ONLY);
+//    	engine.getSettings().setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
+		
+//        ConnectivityManager cm = (ConnectivityManager) this.getSystemService(Activity.CONNECTIVITY_SERVICE);
+//        if(cm.getActiveNetworkInfo().isConnected()){
+//        	engine.getSettings().setCacheMode(WebSettings.LOAD_DEFAULT);
+//        	engine.loadUrl(it.mplink);
+//        }
+//        else{
+//        	engine.getSettings().setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
+//        	engine.loadUrl(it.mplink);
+//        }	
+		
+//		engine.setWebViewClient(new WebViewClient() {
+//		    @Override
+//		    public void onReceivedError(WebView view, int errorCode,
+//		            String description, String failingUrl)
+//		    {
+//		        if (it.mplink.equals(failingUrl))
+//		        {
+//		            Log.e(TAG, "try to load from cache");
+//		            view.getSettings().setCacheMode(WebSettings.LOAD_CACHE_ONLY);
+//		            view.loadUrl(it.mplink);
+//		            return;
+//		        }
+//		        else
+//		        if (it.mplink.equals(failingUrl))
+//		        {
+//		            Log.e(TAG, "cache failed as well, load a local resource as last resort");
+//		            // or inform the user
+//		        }
+//		        super.onReceivedError(view, errorCode, description, failingUrl);
+//		    }
+//		});
+		
+		
+//		engine.loadUrl("file://"+getFilesDir().getAbsolutePath() + File.separator + "my.webarchive");
+//		engine.loadUrl("file:///android_asset/qqq");
+        Log.w(TAG, it.mplink);
+		
+//		HttpClient httpClient = new DefaultHttpClient();
+//		HttpContext localContext = new BasicHttpContext();
+//		HttpGet httpGet = new HttpGet(it.mplink);
+//		String result = "";
+//		try{
+//		HttpResponse response = httpClient.execute(httpGet, localContext);
+//
+//		BufferedReader reader = new BufferedReader(
+//		    new InputStreamReader(
+//		      response.getEntity().getContent()
+//		    )
+//		  );
+//
+//		String line = null;
+//		while ((line = reader.readLine()) != null){
+//		  result += line + "\n";
+//
+//		}
+//		}
+//		catch(Exception e) {
+//			
+//			Log.w(TAG, "ERROR !!!!!!!!!!!!!");
+//		}
+//		
+//		ccc = result;
+		
+		
+//		engine.loadUrl(it.mplink);
+        Log.w(TAG, "CCC = " + ccc);
+//		engine.loadData(ccc, "text/html", "utf-8");
+//		engine.loadDataWithBaseURL(null, ccc, "text/html", "utf-8", null);
 
+//		String htmlData = "<link rel=\"stylesheet\" type=\"text/css\" href=\"style.css\" />" + ccc;
+		// lets assume we have /assets/style.css file
+//		engine.loadDataWithBaseURL("file:///android_asset/", htmlData, "text/html", "UTF-8", null);
+	
+        
+        
+		String htmlData = "<link rel=\"stylesheet\" type=\"text/css\" href=\"style.css\" /> <html>" + ccc + "</html>";
+        engine.loadDataWithBaseURL("file://"+getFilesDir().getAbsolutePath() + File.separator, htmlData, "text/html", "UTF-8", null);
+	}
+	
 	protected void onResume() {
 		
 		super.onResume();
@@ -64,36 +272,11 @@ public class DownloadActivity extends ListActivity {
 			
         	Common.addfav_flag = false;
     		this.getListView().setVisibility(View.GONE);
-    		WebView engine = (WebView) findViewById(R.id.web_engine_fav);
             engine.setVisibility(View.VISIBLE);
-    		engine.getSettings().setJavaScriptEnabled(true);
     		engine.loadUrl(Common.addfav_url);
-    		
-    		pb = (ProgressBar)findViewById(R.id.ProgressBar02);
+//			engine.loadUrl("file://" +getFilesDir().getAbsolutePath() + File.separator  + "ggg.webarchive");
+  		
 //    		pb.setVisibility(View.VISIBLE);
-    		
-    	    engine.setWebChromeClient(new WebChromeClient() {
-    			
-    			public void onProgressChanged(WebView view, int progress) {
-    				
-    				if(progress < 100)
-    					pb.setVisibility(View.VISIBLE);
-    				else
-    					pb.setVisibility(View.GONE);
-    					
-//    				activity.setProgress(progress * 1000);
-//    				Log.w(TAG, ""+progress);
-    			}
-    		});
-    	    
-    	    engine.setWebViewClient(new WebViewClient() {
-    	    	
-    	    	public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
-
-    	    		pb.setVisibility(View.GONE);
-    	    	}
-    	    });
-
 
 		}
 		else {
@@ -116,6 +299,8 @@ public class DownloadActivity extends ListActivity {
     		this.getListView().setVisibility(View.VISIBLE);
     		WebView engine = (WebView) findViewById(R.id.web_engine_fav);
             engine.setVisibility(View.GONE);
+            engine.stopLoading();
+            pb.setVisibility(View.GONE);
             
 	        return true;
         }
--- aLexPro/src/com/vkassin/alexpro/MainActivity.java
+++ aLexPro/src/com/vkassin/alexpro/MainActivity.java
@@ -11,6 +11,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Button;
@@ -18,6 +19,7 @@ import android.widget.ProgressBar;
 import android.widget.TabHost;
 import android.widget.Toast;
 import android.util.Log;
+import android.webkit.WebStorage;
 
 public class MainActivity extends Activity {
 
@@ -39,8 +41,21 @@ public class MainActivity extends Activity {
 	        		setContentView(R.layout.mainact);
 	        	    pb = (ProgressBar)findViewById(R.id.ProgressBar00);
 	        		engine = (WebView) findViewById(R.id.web_engine);
-	        		engine.setWebViewClient(new HelloWebViewClient());
+//	        		engine.setWebViewClient(new HelloWebViewClient());
 	        		engine.getSettings().setJavaScriptEnabled(true);
+	        		engine.getSettings().setDomStorageEnabled(true);
+	        		engine.getSettings().setAppCacheMaxSize(1024*1024*8);
+//	                engine.getSettings().setAppCachePath("/data/data/de.app/cache");
+//	        		engine.getSettings().setAllowFileAccess(true);
+//	        		engine.getSettings().setAppCacheEnabled(true);
+	        		String appCachePath = "/data/data/com.vkassin.alexpro/cache";//getApplicationContext().getCacheDir().getAbsolutePath();
+	        		engine.getSettings().setAppCachePath(appCachePath);
+	        		engine.getSettings().setAllowFileAccess(true);
+	        		engine.getSettings().setAppCacheEnabled(true);
+	        		engine.getSettings().setCacheMode(WebSettings.LOAD_DEFAULT);
+
+	        		
+	        		
 	        		engine.loadUrl(Common.WEB_OPEN);
 //	        		pb.setVisibility(View.VISIBLE);
 	        		final Activity activity = MainActivity.this;
@@ -56,6 +71,13 @@ public class MainActivity extends Activity {
 //	        				activity.setProgress(progress * 1000);
 //	        				Log.w(TAG, ""+progress);
 	        			}
+	                    
+	        			@Override
+	                    public void onReachedMaxAppCacheSize(long spaceNeeded, long totalUsedQuota,
+	                                 WebStorage.QuotaUpdater quotaUpdater)
+	                    {
+	                          quotaUpdater.updateQuota(spaceNeeded * 2);
+	                    }
 	        		});
 	        	    
 	        	    engine.setWebViewClient(new WebViewClient() {
@@ -64,6 +86,19 @@ public class MainActivity extends Activity {
 
 	        	    		pb.setVisibility(View.GONE);
 	        	    	}
+	        	    	
+//	        	        @Override
+//	        	        public void onReachedMaxAppCacheSize(long spaceNeeded, long totalUsedQuota,
+//	        	                     WebStorage.QuotaUpdater quotaUpdater)
+//	        	        {
+//	        	              quotaUpdater.updateQuota(spaceNeeded * 2);
+//	        	        }
+	        	        
+	        		    @Override
+	        		    public boolean shouldOverrideUrlLoading(WebView view, String url) {
+	        		        view.loadUrl(url);
+	        		        return true;
+	        		    }
 	        	    });
 	            }
 	        });
@@ -80,14 +115,14 @@ public class MainActivity extends Activity {
         return super.onKeyDown(keyCode, event);
     }
 	
-	private class HelloWebViewClient extends WebViewClient {
-	    @Override
-	    public boolean shouldOverrideUrlLoading(WebView view, String url) {
-	        view.loadUrl(url);
-	        return true;
-	    }
-
-	}
+//	private class HelloWebViewClient extends WebViewClient {
+//	    @Override
+//	    public boolean shouldOverrideUrlLoading(WebView view, String url) {
+//	        view.loadUrl(url);
+//	        return true;
+//	    }
+//
+//	}
 	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -103,7 +138,7 @@ public class MainActivity extends Activity {
 	        	if(engine.getUrl().indexOf(Common.TEST_STRING1) != -1) {
 	        	
 	        	    Common.addfav_flag = true;
-	        	    Common.addfav_url = engine.getUrl().replaceFirst(Common.TEST_STRING1, Common.TEST_STRING2);
+	        	    Common.addfav_url = /*"http://www.google.com";*/engine.getUrl().replaceFirst(Common.TEST_STRING1, Common.TEST_STRING2);
 
 	        	    RSSItem i = new RSSItem(item_type.IT_KODEKS);
 	        		i.title = engine.getTitle();
